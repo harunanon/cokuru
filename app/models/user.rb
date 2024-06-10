f245e4189903df69646ba41283673afaee3460f3
harunanon <haruna.nobuoka@gmail.com>
2024-06-10 15:09:58 +0900

    コントローラの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :reservations
   has_many :coworking_spaces, through: :reservations
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, 
+  validates :password_digest, presence: true
 end
